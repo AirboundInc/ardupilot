058f81c25d9f1e8ce8238a3e5a2b2efc2979cecd
Mayank Joneja <mayankj@airbound.co>
2025-04-21 18:14:20 +0530

    Add logs for roll and yaw controller

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -1,7 +1,7 @@
 #include "Plane.h"
 
-uint32_t last_log_pitch_ms = 0, last_log_roll_ms = 0, last_log_rudder_ms = 0;
-uint32_t LOGGING_DELAY_MS = 1000; // 1 second delay before logging
+uint32_t last_log_pitch_ms = 0, last_log_roll_ms = 0, last_log_yaw_ms = 0;
+uint32_t LOGGING_DELAY_MS = 2000; // delay for logging rpy controller outputs in milliseconds
 
 /*
   calculate speed scaling number for control surfaces. This is applied
@@ -124,6 +124,13 @@ void Plane::stabilize_roll()
     }
 
     const float roll_out = stabilize_roll_get_roll_out();
+
+    const uint32_t now = millis();
+    if (now - last_log_roll_ms > LOGGING_DELAY_MS) {
+        last_log_roll_ms = now;
+        GCS_SEND_TEXT(MAV_SEVERITY_INFO, "> Roll controller output: %f", roll_out);
+    }
+
     SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, roll_out);
 }
 
@@ -180,7 +187,7 @@ void Plane::stabilize_pitch()
     const uint32_t now = millis();
     if (now - last_log_pitch_ms > LOGGING_DELAY_MS) {
         last_log_pitch_ms = now;
-        GCS_SEND_TEXT(MAV_SEVERITY_INFO, ">>> Pitch controller output: %f", pitch_out);
+        GCS_SEND_TEXT(MAV_SEVERITY_INFO, "> Pitch controller output: %f", pitch_out);
     }
 
     SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, pitch_out);
@@ -370,6 +377,12 @@ void Plane::stabilize_yaw()
       now calculate rudder for the rudder
      */
     const float rudder_output = calc_nav_yaw_coordinated();
+
+    const uint32_t now = millis();
+    if (now - last_log_yaw_ms > LOGGING_DELAY_MS) {
+        last_log_yaw_ms = now;
+        GCS_SEND_TEXT(MAV_SEVERITY_INFO, "> Yaw controller output: %f", rudder_output);
+    }
 
     if (!ground_steering) {
         // Not doing ground steering, output rudder on steering channel
