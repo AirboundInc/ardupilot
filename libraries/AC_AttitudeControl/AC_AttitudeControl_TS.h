94c29aaf30b7f1fc1081a2cec57fa9f6d782ea49
Mayank Joneja <mayankj@airbound.co>
2025-05-15 13:08:27 +0530

    Add stubs to get param values

--- libraries/AC_AttitudeControl/AC_AttitudeControl_TS.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_TS.h
@@ -5,6 +5,11 @@
 
 #include "AC_AttitudeControl_Multi.h"
 
+// TODO: Turn into params
+const float CRAFT_MASS_KG = 1.3;
+const float VECTORING_MIN_ANGLE_DEG = -45.0;
+const float VECTORING_MAX_ANGLE_DEG = 45.0;
+
 class AC_AttitudeControl_TS : public AC_AttitudeControl_Multi
 {
 public:
@@ -20,4 +25,13 @@ public:
 
     // run custom rate loop for tailsitters
     virtual void rate_controller_run() override;
+
+    // wind force boost methods
+    float calculate_thrust(float rpm, float pitch, float tv_angle);
+    float calculate_wind_force(float pitch);
+
+    // Helper methods
+    uint16_t get_servo_min(uint8_t channel);
+    float get_param_value_by_name(char *param_name, float default_value);
+    float pwm_to_angle(uint16_t pwm, uint16_t pwm_min, uint16_t pwm_max);
 };
