c1b5688b3c7be260bd0e6ad159242f0290f4e260
Mayank Joneja <mayankj@airbound.co>
2025-05-16 17:16:32 +0530

    Make accel fnetp fwindp class vars for logging

--- libraries/AC_AttitudeControl/LogStructure.h
+++ libraries/AC_AttitudeControl/LogStructure.h
@@ -48,8 +48,9 @@ struct PACKED log_AttControlTS {
     uint64_t time_us;
     float filt_acc_z;
     float f_net_p;
+    float f_wind_p;
 };
 
 #define LOG_STRUCTURE_FROM_ATTC                         \
     { LOG_ATT_CONTROL_TS_MSG, sizeof(log_AttControlTS), \
-        "ACTS", "Qff", "TimeUS,FiltAccZ,FnetP", "soN", "F--", true },
+        "ACTS", "Qfff", "TimeUS,FiltAccZ,FnetP,FwindP", "soNN", "F---", true },
