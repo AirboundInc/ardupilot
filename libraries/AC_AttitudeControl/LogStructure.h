7145bebf5f44f0e29c280fe08a744728d499058d
Mayank Joneja <mayankj@airbound.co>
2025-05-17 14:37:26 +0530

    Split logs for att controller tailsitter into 2 msgs

--- libraries/AC_AttitudeControl/LogStructure.h
+++ libraries/AC_AttitudeControl/LogStructure.h
@@ -3,47 +3,19 @@
 #include <AP_Logger/LogStructure.h>
 
 #define LOG_IDS_FROM_ATT_CONTROL \
-    LOG_ATT_CONTROL_TS_MSG
+    LOG_ACTS0_MSG,               \
+        LOG_ACTS1_MSG
 
-// @LoggerMessage: ACTS
-// @Description: Attitude Control Tailsitter messages
+// @LoggerMessage: ATS0
+// @Description: Attitude control TailSitter messages 0
 // @Field: TimeUS: Time since system startup
 // @Field: FiltAccZ: Filtered acceleration along Z axis
 // @Field: FnetP: Net perpendicular force
 // @Field: FwindP: Wind perpendicular force
-// @Field: PhiLeft: Thrust vectoring angle left 
+// @Field: PhiLeft: Thrust vectoring angle left
 // @Field: PhiRight: Thrust vectoring angle right
-// @Field: ThrustRight: Thrust from right motor
-// @Field: ThrustRightH: Horizontal component of right thrust
-// @Field: ThrustRightV: Vertical component of right thrust
-// @Field: ThrustRightP: Perpendicular component of right thrust
-// @Field: ThrustLeft: Thrust from left motor
-// @Field: ThrustLeftH: Horizontal component of left thrust
-// @Field: ThrustLeftV: Vertical component of left thrust
-// @Field: ThrustLeftP: Perpendicular component of left thrust
-// @Field: PitchPIDBoostWind: Calculated Pitch PID boost wind
-
-// attitude control tailsitter logging
-// struct PACKED log_AttControlTS {
-//     LOG_PACKET_HEADER;
-//     uint64_t time_us;
-//     float filt_acc_z;
-//     float f_net_p;
-//     float f_wind_p;
-//     float phi_left;
-//     float phi_right;
-//     float thrust_right;
-//     float thrust_right_h;
-//     float thrust_right_v;
-//     float thrust_right_p;
-//     float thrust_left;
-//     float thrust_left_h;
-//     float thrust_left_v;
-//     float thrust_left_p;
-//     float pitch_pid_boost_wind;
-// };
-
-struct PACKED log_AttControlTS {
+// @Field: PIDBoost: Calculated Pitch PID boost wind
+struct PACKED log_ACTS0 {
     LOG_PACKET_HEADER;
     uint64_t time_us;
     float filt_acc_z;
@@ -51,13 +23,37 @@ struct PACKED log_AttControlTS {
     float f_wind_p;
     float phi_left;
     float phi_right;
-
     float pitch_pid_boost_wind;
-    // float thrust_right_h;
-    // float thrust_right_v;
-    // float thrust_right_p;
 };
 
-#define LOG_STRUCTURE_FROM_ATTC                         \
-    { LOG_ATT_CONTROL_TS_MSG, sizeof(log_AttControlTS), \
-        "ACTS", "Qffffff", "TimeUS,FiltAccZ,FnetP,FwindP,PhiLeft,PhiRight,PitchPIDBoostWind", "soNNdd-", "F------", true },
+// @LoggerMessage: ATS1
+// @Description: Attitude control TailSitter messages 1
+// @Field: TimeUS: Time since system startup
+// @Field: ThstL: Thrust from left motor
+// @Field: ThstH: Horizontal component of left thrust
+// @Field: ThstLV: Vertical component of left thrust
+// @Field: ThstLP: Perpendicular component of left thrust
+// @Field: ThstR: Thst from right motor
+// @Field: ThstRH: Horizontal component of right thrust
+// @Field: ThstRV: Vertical component of right thrust
+// @Field: ThstRP: Perpendicular component of right thrust
+struct PACKED log_ACTS1 {
+    LOG_PACKET_HEADER;
+    uint64_t time_us;
+
+    float thrust_left;
+    float thrust_left_h;
+    float thrust_left_v;
+    float thrust_left_p;
+
+    float thrust_right;
+    float thrust_right_h;
+    float thrust_right_v;
+    float thrust_right_p;
+};
+
+#define LOG_STRUCTURE_FROM_ATTC                                                                                    \
+    { LOG_ACTS0_MSG, sizeof(log_ACTS0),                                                                            \
+        "ATS0", "Qffffff", "TimeUS,FiltAccZ,FnetP,FwindP,PhiLeft,PhiRight,PIDBoost", "soNNdd-", "F------", true }, \
+        { LOG_ACTS1_MSG, sizeof(log_ACTS1),                                                                        \
+            "ATS1", "Qffffffff", "TimeUS,ThstL,ThstLH,ThstLV,ThstLP,ThstR,ThstRH,ThstRV,ThstRP", "sNNNNNNNN", "F--------", true },
