31e66d83a39cbca0d464514c0b0dd63532b221c8
anant3110 <anants@airbound.co>
2025-09-03 18:29:36 +0530

    Remove the previous approach, replicate the flow from WheelEncoders

--- libraries/AP_QuadEncoder/AP_QuadEncoder.cpp
+++ libraries/AP_QuadEncoder/AP_QuadEncoder.cpp
@@ -1,65 +1,82 @@
 #include <AP_HAL/AP_HAL.h>
-extern const AP_HAL::HAL& hal;
 
 #include "AP_QuadEncoder.h"
 
-const int8_t AP_QuadEncoder::_lookupTable[16] = {
+#include <GCS_MAVLink/GCS.h>
+
+extern const AP_HAL::HAL& hal;
+
+const int8_t AP_Quadrature::_lookupTable[16] = {
     0,  -1,   1,   0,
     1,   0,   0,  -1,
    -1,   0,   0,   1,
     0,   1,  -1,   0
 };
 
-AP_QuadEncoder* AP_QuadEncoder::_left_instance = nullptr;
-AP_QuadEncoder* AP_QuadEncoder::_right_instance = nullptr;
-
-AP_QuadEncoder::AP_QuadEncoder(uint8_t pinA, uint8_t pinB, int32_t cpr, Side side)
-    : _pinA(pinA), _pinB(pinB), _cpr(cpr), _position(0), _prevAB(0), _side(side)
+void AP_Quadrature::update_pin(uint8_t &pin, uint8_t new_pin, uint8_t &pin_value)
 {
-    if (_side == LEFT) {
-        _left_instance = this;
-    } else {
-        _right_instance = this;
+    if (new_pin == pin) {
+        // no change
+        return;
+    }
+
+    // remove old gpio event callback if present
+    if (pin != (uint8_t)-1 &&
+        !hal.gpio->detach_interrupt(pin)) {
+        GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "QEnc: Failed to detach from pin %u", pin);
+        // ignore this failure or the user may be stuck
+    }
+
+    pin = new_pin;
+
+    // install interrupt handler on rising or falling edge of gpio for pin a
+    if (new_pin != (uint8_t)-1) {
+        hal.gpio->pinMode(pin, HAL_GPIO_INPUT);
+        if (!hal.gpio->attach_interrupt(
+                pin,
+                FUNCTOR_BIND_MEMBER(&AP_Quadrature::irq_handler, void, uint8_t, bool, uint32_t), AP_HAL::GPIO::INTERRUPT_BOTH)) {
+            GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "WEnc: Failed to attach to pin %u", pin);
+        }
+        pin_value = hal.gpio->read(pin);
     }
 }
 
-void AP_QuadEncoder::init() {
-    auto& gpio = hal.gpio;
-    gpio->pinMode(_pinA, HAL_GPIO_INPUT);
-    gpio->pinMode(_pinB, HAL_GPIO_INPUT);
-
-    _prevAB = (gpio->read(_pinA) << 1) | gpio->read(_pinB);
-
-    if (_side == LEFT) {
-        gpio->attach_interrupt(_pinA, isr_left, AP_HAL::GPIO::INTERRUPT_BOTH);
-        gpio->attach_interrupt(_pinB, isr_left, AP_HAL::GPIO::INTERRUPT_BOTH);
-    } else {
-        gpio->attach_interrupt(_pinA, isr_right, AP_HAL::GPIO::INTERRUPT_BOTH);
-        gpio->attach_interrupt(_pinB, isr_right, AP_HAL::GPIO::INTERRUPT_BOTH);
+void AP_Quadrature::irq_handler(uint8_t pin, bool pin_value, uint32_t timestamp)
+{
+    uint8_t index = 0;
+    if(pin == _pinA) {
+        index = pin_value << 3 | B_value << 2 | A_value << 1 | B_value;
+        A_value = pin_value;
     }
+
+    else if(pin == _pinB) {
+        index = A_value << 3 | pin_value << 2 | B_value << 1 | A_value;
+        B_value = pin_value;
+    }
+
+    else {
+        return;
+    }
+
+    irq_state.phase = _lookupTable[index];
+    irq_state.angle = irq_state.phase * (360.0f / _cpr);
+    irq_state.last_reading_ms = timestamp * 1e-3f;
+
 }
 
-void AP_QuadEncoder::isr_left() {
-    if (_left_instance) _left_instance->isr();
-}
-void AP_QuadEncoder::isr_right() {
-    if (_right_instance) _right_instance->isr();
-}
+void AP_Quadrature::update(void)
+{
+    update_pin(_pinA, get_pin_a(), A_value);
+    update_pin(_pinB, get_pin_b(), B_value);
 
-void AP_QuadEncoder::isr() {
-    auto& gpio = hal.gpio;
-    uint8_t a = gpio->read(_pinA);
-    uint8_t b = gpio->read(_pinB);
-    uint8_t currAB = (a << 1) | b;
-    uint8_t index = (_prevAB << 2) | currAB;
-    _position += _lookupTable[index];
-    _prevAB = currAB;
-}
+    // disable interrupts to prevent race with irq_handler
+    void *irqstate = hal.scheduler->disable_interrupts_save();
 
-int32_t AP_QuadEncoder::get_position() const {
-    return _position;
-}
+    // copy phase, angle and last timestamp so it is accessible to front end
+    copy_state_to_frontend(irq_state.phase,
+                           irq_state.angle,
+                           irq_state.last_reading_ms);
 
-float AP_QuadEncoder::get_angle() const {
-    return (float(_position) / float(_cpr)) * 360.0f;
+    // restore interrupts
+    hal.scheduler->restore_interrupts(irqstate);
 }
