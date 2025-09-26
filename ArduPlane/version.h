01f24aedcc60564deec37f0534a145e02693233f
Mayank Joneja <mayankj@airbound.co>
2025-09-26 17:26:36 +0530

    Change versiont to rc8

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.5.7.1 - rc7"
+#define THISFIRMWARE "ArduPlane V4.5.7.1 - rc8"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_DEV
