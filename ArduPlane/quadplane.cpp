34ea6a89af88bfce6efd5bcb2ed1d5a46aacf638
anant3110 <anants@airbound.co>
2025-10-09 14:20:20 +0530

    Make RENC_ to RE_ in param definition

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -552,6 +552,12 @@ const AP_Param::GroupInfo QuadPlane::var_info2[] = {
     // @Increment: 0.1
     // @User: Advanced
     AP_GROUPINFO("LND_FRZ_TIM", 39, QuadPlane, q_land_freeze_time, 7.0f),
+    
+#if HAL_QUADPLANE_ENABLED
+    // @Group: RE_
+    // @Path: ../libraries/AP_RotaryEncoder/AP_RotaryEncoder.cpp
+    AP_SUBGROUPINFO(rotary_encoder, "RE_", 40, QuadPlane, AP_RotaryEncoder), 
+#endif
 
     AP_GROUPEND
 };
