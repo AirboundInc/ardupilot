913e220cf9c7d4ee955cd5b45d330d93339f5d12
anant3110 <anants@airbound.co>
2025-10-22 15:56:25 +0530

    Add Rotary Encoder driver based on AP_WheelEncoder on Rover

--- ArduPlane/Log.cpp
+++ ArduPlane/Log.cpp
@@ -34,13 +34,14 @@ void Plane::Log_Write_Attitude(void)
         logger.Write_PID(LOG_PIQY_MSG, quadplane.attitude_control->get_rate_yaw_pid().get_pid_info());
         logger.Write_PID(LOG_PIQA_MSG, quadplane.pos_control->get_accel_z_pid().get_pid_info() );
 
-        // Write tailsitter specific log at same rate as PIDs
-        quadplane.tailsitter.write_log();
     }
     if (quadplane.in_vtol_mode() && quadplane.pos_control->is_active_xy()) {
         logger.Write_PID(LOG_PIDN_MSG, quadplane.pos_control->get_vel_xy_pid().get_pid_info_x());
         logger.Write_PID(LOG_PIDE_MSG, quadplane.pos_control->get_vel_xy_pid().get_pid_info_y());
     }
+
+    // Write tailsitter specific log at same rate as PIDs
+    quadplane.tailsitter.write_log();
 #endif
 
     logger.Write_PID(LOG_PIDR_MSG, rollController.get_pid_info());
@@ -441,6 +442,16 @@ const struct LogStructure Plane::log_structure[] = {
 #if HAL_QUADPLANE_ENABLED
     { LOG_TSIT_MSG, sizeof(Tailsitter::log_tailsitter),
       "TSIT", "Qfff",  "TimeUS,Ts,Ss,Tmin", "s---", "F---" , true },
+#endif
+
+// @LoggerMessage: RENC
+// @Description: Rotary Encoder values for the tailsitter thrust vectors
+// @Field: TimeUS: time of the log
+// @Field: PosL: angle on the left thrust vector
+// @Field: PosR: angle on the right thrust vector
+#if HAL_QUADPLANE_ENABLED
+    { LOG_ROTARYENCODER_MSG, sizeof(log_RotaryEncoder),
+      "RENC", "Qff", "TimeUS,PosL,PosR", "sdd", "F--", true },
 #endif
 
 // @LoggerMessage: TILT
---
+++ libraries/AP_RotaryEncoder/AP_RotaryEncoder_Backend.h
@@ -0,0 +1,42 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <AP_Common/AP_Common.h>
+#include "AP_RotaryEncoder.h"
+
+class AP_RotaryEncoder_Backend
+{
+public:
+    // constructor. This incorporates initialisation as well.
+    AP_RotaryEncoder_Backend(AP_RotaryEncoder &frontend, uint8_t instance, AP_RotaryEncoder::RotaryEncoder_State &state);
+
+    // we declare a virtual destructor so that RotaryEncoder drivers can
+    // override with a custom destructor if need be
+    virtual ~AP_RotaryEncoder_Backend(void) {}
+
+    // update the state structure. All backends must implement this.
+    virtual void update() = 0;
+
+protected:
+
+    // return pin number.  returns -1 if pin is not defined for this instance
+    int8_t get_pin_a() const;
+    int8_t get_pin_b() const;
+
+    // copy state to front end helper function
+    void copy_state_to_frontend(int32_t count, uint32_t angle, uint32_t time);
+
+    AP_RotaryEncoder &_frontend;
+    AP_RotaryEncoder::RotaryEncoder_State &_state;
+};
