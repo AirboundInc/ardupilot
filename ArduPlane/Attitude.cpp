3c29103ceede41eb439d470415603c0ed5e2b272
akshar <aksharp@airbound.co>
2025-11-14 21:53:54 -0800

    Update last_pos2_ms time for non-rtl modes to freeze climb rate

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -121,7 +121,6 @@ void Plane::stabilize_roll()
     }
 
     const float roll_out = stabilize_roll_get_roll_out();
-
     SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, roll_out);
 }
 
@@ -175,7 +174,6 @@ void Plane::stabilize_pitch()
     }
 
     const float pitch_out = stabilize_pitch_get_pitch_out();
-
     SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, pitch_out);
 }
 
