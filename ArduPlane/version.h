c7e94f1571a65bfbd48133ba0aa929c25de31770
akshar <aksharp@airbound.co>
2025-12-18 01:40:33 -0800

    Update version.h Enable PPP networking support Added auto door control lua script Added auto control surface check lua script

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Airbound ArduPlane V4.6.3.1 rc1 - dcm airspeed estimate"
+#define THISFIRMWARE "Airbound ArduPlane V4.6.3.1 - rc2"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,3,FIRMWARE_VERSION_TYPE_DEV
