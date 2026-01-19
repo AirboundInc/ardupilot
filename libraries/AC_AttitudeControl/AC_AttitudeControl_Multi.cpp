3d1e1f1e6d4686ab00c04cd599fec43c2be90fba
Manuraj P M <manurajp@airbound.co>
2026-01-19 14:48:28 +0530

    Attitude_Control: Updated the compensation gain

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
@@ -401,12 +401,9 @@ float AC_AttitudeControl_Multi::get_throttle_boosted_ts(float throttle_in)
         return throttle_in;
     }
     // math and geometry behind this idea is given in https://www.notion.so/airbound/Decoupling-the-vertical-and-Attitude-controllers-2c321adf4be980b89fa5cae73ac286fb?source=copy_link
-
-    float term_1 =  _ahrs.cos_roll()*_ahrs.sin_pitch()*_ahrs.cos_yaw() + _ahrs.sin_roll()*_ahrs.sin_yaw();
-    float term_2 =  _ahrs.cos_roll()*_ahrs.cos_pitch();
     float tilt_angle_rad = constrain_float(radians(_ts_tilt_angle/100.0f), -radians(45.0f), radians(45.0f)); // convert to radians and constrain between 0 and 90 degrees 
     // gcs().send_text(MAV_SEVERITY_WARNING, "Tilt Angle: %f, %f",_ts_tilt_angle/100.0f, tilt_angle_rad*180.0f/3.14f);
-    float inv_compensation_gain =  term_1 * sinf(tilt_angle_rad) + term_2 * cosf(tilt_angle_rad);
+    float inv_compensation_gain =  _ahrs.sin_pitch() * sinf(tilt_angle_rad) + _ahrs.cos_pitch()*_ahrs.cos_roll() * cosf(tilt_angle_rad);
     float boost_factor = 1.0f / constrain_float(inv_compensation_gain, 0.1f, 1.0f);
     float throttle_out = throttle_in * boost_factor;
     _angle_boost = constrain_float(throttle_out - throttle_in, -1.0f, 1.0f);
