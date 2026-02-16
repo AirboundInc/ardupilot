41502065ba7c1d94621eb9c124012cd06543cf7d
Manuraj P M <manurajp@airbound.co>
2026-02-16 14:28:01 +0530

    Tailsitter:Disable the sitl flag

--- ArduPlane/tailsitter.h
+++ ArduPlane/tailsitter.h
@@ -19,7 +19,7 @@
 #include <AP_Motors/AP_MotorsTailsitter.h>
 #include <AP_Logger/LogStructure.h>
 #include <AP_ESC_Telem/AP_ESC_Telem.h>
-#define SITL_DEBUG
+// #define SITL_DEBUG
 
 class QuadPlane;
 class AP_MotorsMulticopter;
