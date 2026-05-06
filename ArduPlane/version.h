4544b3ca88edda4804f37664921d370d5d887bb4
Tarun <tarunn@airbound.co>
2026-05-06 11:12:38 +0530

    Update version.h to 4.5.7.5 RC1 Level Transition

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Airbound ArduPlane V4.5.7.4"
+#define THISFIRMWARE "Airbound ArduPlane V4.5.7.5 rc1 Level Transition"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_DEV
