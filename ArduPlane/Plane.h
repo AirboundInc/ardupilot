f006f653ffbbb4eb16aeaf420b0dbbb9631977ff
akshar-airbound <aksharp@airbound.com>
2026-06-03 13:05:48 +0530

    Add parachute_release_with_disarm
    (cherry picked from commit 7d25d82b8d68e79a8c548051f242c217ef39ad61)

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -1150,6 +1150,7 @@ private:
     void do_parachute(const AP_Mission::Mission_Command& cmd);
     void parachute_release();
     bool parachute_manual_release();
+    bool parachute_release_with_disarm();
 #endif
 
     // soaring.cpp
