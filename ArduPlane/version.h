97c4bbaf1841ee09f2ab677e537846d4c6251c69
akshar-airbound <aksharp@airbound.com>
2026-06-08 16:49:52 +0530

    Updated version.h autobailout.lua: Changed AUTOB_PIT_TOUT : 200 -> 100

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Airbound ArduPlane V4.5.7.4 -hf4 - Controller desaturation"
+#define THISFIRMWARE "Airbound V4.5.7.4 - hf5-ctrldesat-autob"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_DEV
