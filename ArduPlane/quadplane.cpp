0997dcb25a5c9c686e8c2d9a446ffc0243f9cbbb
Mayank Joneja <mayankj@airbound.co>
2026-06-09 15:45:38 +0530

    Reduce weathervane gain during pos1 pos2 repositioning

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -2925,6 +2925,7 @@ void QuadPlane::vtol_position_controller(void)
             if (tailsitter.enabled()){
                 set_climb_rate_cms(0);
                 last_pos2_ms = now_ms;
+                weathervane->set_gain(tailsitter.wvane_max_gain/3);
             }else{
                 Location loc2 = loc;
                 loc2.change_alt_frame(Location::AltFrame::ABOVE_ORIGIN);
@@ -2935,6 +2936,7 @@ void QuadPlane::vtol_position_controller(void)
         } else {
             if (tailsitter.enabled()){
                 last_pos2_ms = now_ms;
+                weathervane->set_gain(tailsitter.wvane_max_gain/3);
             }
             set_climb_rate_cms(0);
         }
