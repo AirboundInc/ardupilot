8714ee15fa883208ee77f6d0f98b56e48b7aa2ec
vigneshm <vigneshm@airbound.co>
2026-06-10 06:54:14 +0000

    removed duplicate declarations in lua_bindings

--- libraries/AP_Scripting/lua_bindings.h
+++ libraries/AP_Scripting/lua_bindings.h
@@ -30,7 +30,4 @@ int lua_range_finder_handle_script_msg(lua_State *L);
 int lua_get_rate_pid_info(lua_State *L);
 int lua_get_att_target_euler_cd(lua_State *L);
 int lua_get_rate_ef_targets(lua_State *L);
-int lua_get_rate_pid_info(lua_State *L);
-int lua_get_att_target_euler_cd(lua_State *L);
-int lua_get_rate_ef_targets(lua_State *L);
 int lua_get_actual_euler_cd(lua_State *L);
