46e62fd2897a65f69df7bb7e0f45012de3e12270
Manuraj P M <manurajp@airbound.co>
2026-06-18 17:02:44 +0530

    Remove the GCS command

--- libraries/AP_Motors/AP_MotorsTailsitter.cpp
+++ libraries/AP_Motors/AP_MotorsTailsitter.cpp
@@ -226,7 +226,6 @@ void AP_MotorsTailsitter::output_armed_stabilizing()
     }
     _tilt_left  = pitch_thrust - yaw_thrust_limited;
     _tilt_right = pitch_thrust + yaw_thrust_limited;
-    gcs().send_text(MAV_SEVERITY_INFO, "Running");
     AP::logger().WriteStreaming("THRD", "TimeUS,ThLP,ThCom,ThMax,ThMi,ThAd,ThOu,ThQt",
         "s-------", // seconds,
         "F0000000", // micro (1e-6), no mult (1e0)
