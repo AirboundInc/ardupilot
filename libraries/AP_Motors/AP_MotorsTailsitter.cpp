26cb3a742c9efb9e4abb127a5c788d5b5b38d524
Manuraj P M <manurajp@airbound.co>
2026-06-30 14:11:34 +0530

    Add motor actuator effort

--- libraries/AP_Motors/AP_MotorsTailsitter.cpp
+++ libraries/AP_Motors/AP_MotorsTailsitter.cpp
@@ -111,12 +111,12 @@ void AP_MotorsTailsitter::output_to_motors()
     SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorLeft, _tilt_left*SERVO_OUTPUT_RANGE);
     SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorRight, _tilt_right*SERVO_OUTPUT_RANGE);
     float thrust_lin = thr_lin.apply_thrust_curve_and_volt_scaling(_throttle);
-     AP::logger().WriteStreaming("THRE", "TimeUS,ThAETR,ThLin",
-        "s--", // seconds,
-        "F00", // micro (1e-6), no mult (1e0)
-        "Qff", // uint64_t, float
+     AP::logger().WriteStreaming("THRE", "TimeUS,ThAETR,ThLin,tl,tr",
+        "s----", // seconds,
+        "F0000", // micro (1e-6), no mult (1e0)
+        "Qffff", // uint64_t, float
         AP_HAL::micros64(),
-        _actuator[2],thrust_lin);
+        _actuator[2],thrust_lin,_actuator[0],_actuator[1]);
 
 }
 
