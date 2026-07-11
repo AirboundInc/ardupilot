81c26094061036395651b56cc72700aad98670dc
akshar-airbound <aksharp@airbound.com>
2026-07-11 14:23:49 +0530

    Update the version to AB ArduPlane V4.5.7.5 - rc6-fixes-rll-gain Reduce the Roll angle P gain to 3.75

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "AB ArduPlane V4.5.7.5 - rc6-fixes-autobv4"
+#define THISFIRMWARE "AB ArduPlane V4.5.7.5 - rc6-fixes-rll-gain"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_DEV
